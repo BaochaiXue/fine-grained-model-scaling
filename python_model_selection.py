107998bc40d50f4db908ea6ea6747ffda8393479
ZHANG Xinjie <xinjzhang8-c@my.cityu.edu.hk>
2024-06-13 06:03:27 +0800

    fix bugs

--- python_model_selection.py
+++ python_model_selection.py
@@ -14,42 +14,52 @@ import os
 import shutil
 import argparse
 
-creator.create(
-    "FitnessMulti", base.Fitness, weights=(-1.0, 20000.0, 2000.0)
-)  # Minimize storage, maximize inference time difference and accuracy
+creator.create("FitnessMulti", base.Fitness, weights=(1.0, 1.0, -1.0))
 creator.create("Individual", list, fitness=creator.FitnessMulti)
 
 
 def select_models(group: pd.DataFrame, K: int, S: float) -> pd.DataFrame:
+    storage_min: float = group["model_size"].min()
+    storage_max: float = group["model_size"].max()
+    inference_min: float = group["inference_time"].min()
+    inference_max: float = group["inference_time"].max()
+    accuracy_min: float = group["accuracy"].min()
+    accuracy_max: float = group["accuracy"].max()
+
+    # Normalize the data using MinMax scaling
+    group["model_size"] = (group["model_size"] - storage_min) / (
+        storage_max - storage_min
+    )
+    group["inference_time"] = (group["inference_time"] - inference_min) / (
+        inference_max - inference_min
+    )
+    group["accuracy"] = (group["accuracy"] - accuracy_min) / (
+        accuracy_max - accuracy_min
+    )
+
     def evaluate(
         individual: List[int], models_df: pd.DataFrame
     ) -> Tuple[float, float, float]:
         selected_models: pd.DataFrame = models_df.iloc[individual]
         total_storage: float = selected_models["model_size"].sum()
-        min_inference_diff: float = np.min(
-            np.diff(sorted(selected_models["inference_time"]))
-        )
+        min_inference_diff: float = np.diff(
+            sorted(selected_models["inference_time"])
+        ).min()
         accuracy_sum: float = selected_models["accuracy"].sum()
-        # penalize for every model that has not an accuracy of at least 0.5
-        for i in range(K):
-            if selected_models.iloc[i]["accuracy"] < 0.5:
-                accuracy_sum *= selected_models.iloc[i]["accuracy"]
-        # bonus for inference time difference
-        max_inference_diff: float = np.max(
-            np.diff(sorted(selected_models["inference_time"]))
-        )
-        diff_ratio: float = min_inference_diff / max_inference_diff
-        min_inference_diff *= 1 + diff_ratio
-        return total_storage, min_inference_diff, accuracy_sum
+        return (min_inference_diff, accuracy_sum, total_storage)
 
     def feasible(individual: List[int], models_df: pd.DataFrame, S: float) -> bool:
+        normalized_S: float = (S - storage_min) / (storage_max - storage_min)
         selected_models: pd.DataFrame = models_df.iloc[individual]
         total_storage: float = selected_models["model_size"].sum()
-        return total_storage <= S and len(set(individual)) == len(individual)
+        if selected_models["accuracy"].min() < (15 - accuracy_min) / (
+            accuracy_max - accuracy_min
+        ):
+            return False
+        return total_storage <= normalized_S and len(set(individual)) == len(individual)
 
     toolbox: base.Toolbox = base.Toolbox()
     toolbox.register("indices", random.sample, range(len(group)), K)
-
     toolbox.register(
         "individual", tools.initIterate, creator.Individual, toolbox.indices
     )
@@ -59,30 +69,41 @@ def select_models(group: pd.DataFrame, K: int, S: float) -> pd.DataFrame:
     toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.2)
     toolbox.register("select", tools.selNSGA2)
 
-    # Define the constraint
-    feasibility_func = lambda ind: feasible(ind, group, S)
+    feasibility_func: typing.Callable = lambda ind: feasible(ind, group, S)
     toolbox.decorate(
         "evaluate",
         tools.DeltaPenalty(
-            feasibility_func, (float("inf"), -float("inf"), -float("inf"))
+            feasibility_func, (-float("inf"), -float("inf"), float("inf"))
         ),
     )
 
     population: List[creator.Individual] = toolbox.population(n=1000)
+    hof: tools.HallOfFame = tools.HallOfFame(1)
     algorithms.eaMuPlusLambda(
         population,
         toolbox,
-        mu=1000,
-        lambda_=2000,
+        mu=100,
+        lambda_=200,
         cxpb=0.7,
         mutpb=0.3,
-        ngen=1000,
+        ngen=100,
         verbose=False,
+        halloffame=hof,
     )
-    ind: creator.Individual
-    population.sort(key=lambda ind: ind.fitness.values)
 
-    best_individual: creator.Individual = population[0]
+    best_individual: List[int] = hof[0]
+
+    print(f"Best individual: {best_individual}")
+
+    # Denormalize the data
+    group["model_size"] = (
+        group["model_size"] * (storage_max - storage_min) + storage_min
+    )
+    group["inference_time"] = (
+        group["inference_time"] * (inference_max - inference_min) + inference_min
+    )
+    group["accuracy"] = group["accuracy"] * (accuracy_max - accuracy_min) + accuracy_min
+
     best_models: pd.DataFrame = group.iloc[best_individual[:K]]
 
     return best_models
@@ -120,7 +141,6 @@ def copy_model(model_name: str, pruning_factor: float, selected_dir: str) -> Non
 def main(S: float, K: int) -> None:
     model_info: pd.DataFrame = pd.read_csv("model_information.csv")
     model_info.set_index(["model", "prune_rate"], inplace=True)
-    model_info.sort_values(by=["model", "inference_time"], inplace=True)
     model_info["model_size"] = model_info["model_size"] / 1024 / 1024
     model_groups: typing.Dict[str, pd.DataFrame] = dict(
         tuple(model_info.groupby("model"))
@@ -129,11 +149,7 @@ def main(S: float, K: int) -> None:
     selected_models_list: List[pd.DataFrame] = []
 
     for model, group in model_groups.items():
-        selected_models: pd.DataFrame = select_models(
-            group,
-            K,
-            S,
-        )
+        selected_models: pd.DataFrame = select_models(group, K, S)
         selected_models_list.append(selected_models)
 
     all_selected_models: pd.DataFrame = pd.concat(selected_models_list)
